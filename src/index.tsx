5f64c22c3933ac589e4dee556dd9428b25af284b
brendancrabill <98237875+brendancrabill@users.noreply.github.com>
2022-11-13 19:01:43 -0500

    Update index.tsx

--- src/index.tsx
+++ src/index.tsx
@@ -1,34 +1,25 @@
-{
-  "name": "react-typescript",
-  "version": "1.0.0",
-  "description": "React and TypeScript example starter project",
-  "keywords": [
-    "typescript",
-    "react",
-    "starter"
-  ],
-  "main": "src/index.tsx",
-  "dependencies": {
-    "function-plot": "1.23.1",
-    "react": "16.13.1",
-    "react-dom": "^16.12.0",
-    "react-scripts": "3.3.0"
-  },
-  "devDependencies": {
-    "@types/react": "16.9.19",
-    "@types/react-dom": "16.9.5",
-    "typescript": "3.7.5"
-  },
-  "scripts": {
-    "start": "react-scripts start",
-    "build": "react-scripts build",
-    "test": "react-scripts test --env=jsdom",
-    "eject": "react-scripts eject"
-  },
-  "browserslist": [
-    ">0.2%",
-    "not dead",
-    "not ie <= 11",
-    "not op_mini all"
+import functionPlot from "function-plot";
+
+let contentsBounds = document.body.getBoundingClientRect();
+let width = 800;
+let height = 500;
+let ratio = contentsBounds.width / width;
+width *= ratio;
+height *= ratio;
+
+functionPlot({
+  target: "#root",
+  width,
+  height,
+  yAxis: { domain: [-1, 9] },
+  grid: true,
+  data: [
+    {
+      fn: "x^2",
+      derivative: {
+        fn: "2 * x",
+        updateOnMouseMove: true
+      }
+    }
   ]
-}
+});
